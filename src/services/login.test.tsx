fef0b55d9b2bccfeffc3902088ff6b904538048a
elissandroa <elissandroa@hotmail.com>
2024-12-12 16:47:14 -0300

    refactoring tests to password validation

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -3,13 +3,14 @@ import { login } from "./login"
 describe('login', () => {
 
     const mockEmail = 'teste@gmail.com'
+    const mockPassword = '123456';
     it('Deve exibir um alert com boas vindas caso o email seja válido', async() => {
-        const response = await login(mockEmail)
+        const response = await login(mockEmail, mockPassword)
         expect(response).toBeTruthy()
     })
 
     it('Deve exibir um erro caso o email seja inválido', async() => {
-        const response = await login('email@invalido.com')
+        const response = await login('email@invalido.com', "@")
         expect(response).toBeFalsy()
     })
 })
